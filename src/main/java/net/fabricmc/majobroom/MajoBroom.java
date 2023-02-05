50f0182e54ce62cca0ed39ecb1d3a103706587b6
soloopooo <soloopooo@foxmail.com>
2023-02-05 22:38:24 +0800

    bump to 1.19.3

--- src/main/java/net/fabricmc/majobroom/MajoBroom.java
+++ src/main/java/net/fabricmc/majobroom/MajoBroom.java
@@ -1,9 +1,11 @@
 package net.fabricmc.majobroom;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.fabricmc.fabric.impl.item.group.FabricCreativeGuiComponents;
+import net.fabricmc.fabric.impl.client.itemgroup.FabricCreativeGuiComponents;
 import net.fabricmc.majobroom.armors.ArmorFabric;
 import net.fabricmc.majobroom.armors.BaseArmor;
 import net.fabricmc.majobroom.config.MajoBroomConfig;
@@ -19,23 +21,40 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
 
 public class MajoBroom implements ModInitializer {
 	public static final String MODID = "majobroom";
-	public static final ItemGroup majoGroup = FabricItemGroupBuilder.build(new Identifier("majobroom", "majo_group"), () -> new ItemStack(MajoBroom.broomItem));
+	public static final ItemGroup majoGroup = FabricItemGroup.builder(new Identifier(MODID, "majo_group"))
+		.icon(() -> new ItemStack(MajoBroom.broomItem)).build();
+	
 
 	//盔甲部分
 	public static final ArmorMaterial FABRIC_ARMOR = new ArmorFabric();
-	public static final Item broomItem = new BroomItem(new Item.Settings().group(MajoBroom.majoGroup).maxCount(1));
+	public static final Item broomItem = new BroomItem(new Item.Settings().maxCount(1));
+//	group(MajoBroom.majoGroup)
 	public static final Item majoCloth = new BaseArmor(FABRIC_ARMOR, EquipmentSlot.CHEST);
 //	public static final Item majoStocking = new BaseArmor(FABRIC_ARMOR, EquipmentSlot.FEET);
 	public static final Item majoHat = new BaseArmor(FABRIC_ARMOR, EquipmentSlot.HEAD);
 
 
+	//ItemGroup
+	static {
+		ItemGroupEvents.modifyEntriesEvent(majoGroup).register(MajoBroom::setItemGroup);
+	}
+	
+	protected static void setItemGroup(FabricItemGroupEntries entries) {
+		entries.add(broomItem);
+		entries.add(majoHat);
+		entries.add(majoCloth);
+		//entries.add(majoStocking);
+	}
+
 	//实体
 	public static final EntityType<BroomEntity> BROOM_ENTITY_ENTITY_TYPE = Registry.register(
-			Registry.ENTITY_TYPE,
+			Registries.ENTITY_TYPE,
 			new Identifier(MODID, "majo_broom"),
 			FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, BroomEntity::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build()
 	);
@@ -43,14 +62,13 @@ public class MajoBroom implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		Registry.register(Registry.ITEM, new Identifier(MODID, "broom_item"), broomItem);
-		Registry.register(Registry.ITEM, new Identifier(MODID, "majo_cloth"), majoCloth);
-//		Registry.register(Registry.ITEM, new Identifier(MODID, "stocking"), majoStocking);
-		Registry.register(Registry.ITEM, new Identifier(MODID, "majo_hat"), majoHat);
+		Registry.register(Registries.ITEM, new Identifier(MODID, "broom_item"), broomItem);
+		Registry.register(Registries.ITEM, new Identifier(MODID, "majo_cloth"), majoCloth);
+//		Registry.register(Registries.ITEM, new Identifier(MODID, "stocking"), majoStocking);
+		Registry.register(Registries.ITEM, new Identifier(MODID, "majo_hat"), majoHat);
 //		FabricDefaultAttributeRegistry.register(CUBE, CubeEntity);
-//		Registry.register(Registry.ITEM, new Identifier(MODID, "fabric_helmet"), new BaseArmor(FABRIC_ARMOR, EquipmentSlot.HEAD));
+//		Registry.register(Registries.ITEM, new Identifier(MODID, "fabric_helmet"), new BaseArmor(FABRIC_ARMOR, EquipmentSlot.HEAD));
 		MajoBroomConfig.getInstance();
-
 
 	}
 
